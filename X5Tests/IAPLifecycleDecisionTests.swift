38f74cc293e81d86eaa106210ecd4d1d5f3d5613
tuako <tuakov.ursa@gmail.com>
2026-07-15 01:14:48 +0500

    test: require server verification for legacy Apple tokens

--- X5Tests/IAPLifecycleDecisionTests.swift
+++ X5Tests/IAPLifecycleDecisionTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import XCTest
 @testable import X5
 
@@ -30,5 +31,26 @@ final class IAPLifecycleDecisionTests: XCTestCase {
 
     func testNonAuthorizationFailureDoesNotRefreshToken() {
         XCTAssertFalse(IAPVerificationRetryPolicy.shouldRetry(statusCode: 500, retryCount: 0))
+    }
+
+    func testMismatchedLegacyTokenIsDeferredToServerOwnershipVerification() {
+        let signedInUserID = "11111111-1111-4111-8111-111111111111"
+        let historicalToken = UUID(uuidString: "22222222-2222-4222-8222-222222222222")
+
+        XCTAssertTrue(
+            IAPOwnershipRoutingPolicy.shouldVerifyOnServer(
+                signedInUserID: signedInUserID,
+                transactionAppAccountToken: historicalToken
+            )
+        )
+    }
+
+    func testOwnershipVerificationDoesNotStartWithoutSignedInUser() {
+        XCTAssertFalse(
+            IAPOwnershipRoutingPolicy.shouldVerifyOnServer(
+                signedInUserID: nil,
+                transactionAppAccountToken: nil
+            )
+        )
     }
 }
