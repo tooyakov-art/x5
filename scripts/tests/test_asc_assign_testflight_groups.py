ac789504df96c9ba67882bb73620fe04be141831
tuako <tuakov.ursa@gmail.com>
2026-07-28 19:12:55 +0500

    chore: authorize TestFlight build 192 groups

--- scripts/tests/test_asc_assign_testflight_groups.py
+++ scripts/tests/test_asc_assign_testflight_groups.py
@@ -47,9 +47,9 @@ class AssignTestFlightGroupsContractTests(unittest.TestCase):
                 self.app_id = app_id
                 return [
                     {
-                        "id": "build-191",
+                        "id": "build-192",
                         "attributes": {
-                            "version": "191",
+                            "version": "192",
                             "processingState": "VALID",
                             "expired": False,
                         },
@@ -87,13 +87,13 @@ class AssignTestFlightGroupsContractTests(unittest.TestCase):
         result = module.assign_internal_groups(
             client,
             bundle_id="com.x5studio.app",
-            build_number="191",
+            build_number="192",
             group_names=("123", "321"),
         )
 
         self.assertEqual(
             client.added,
-            [("group-123", "build-191"), ("group-321", "build-191")],
+            [("group-123", "build-192"), ("group-321", "build-192")],
         )
         self.assertEqual(result, {"123": "confirmed", "321": "confirmed"})
 
@@ -101,7 +101,7 @@ class AssignTestFlightGroupsContractTests(unittest.TestCase):
         module.assign_internal_groups(
             client,
             bundle_id="com.x5studio.app",
-            build_number="191",
+            build_number="192",
             group_names=("123", "321"),
         )
         self.assertEqual(len(client.added), 2)
@@ -124,9 +124,9 @@ class AssignTestFlightGroupsContractTests(unittest.TestCase):
             def list_builds(self, _app_id):
                 return [
                     {
-                        "id": "build-191",
+                        "id": "build-192",
                         "attributes": {
-                            "version": "191",
+                            "version": "192",
                             "processingState": "VALID",
                             "expired": False,
                         },
@@ -164,7 +164,7 @@ class AssignTestFlightGroupsContractTests(unittest.TestCase):
             module.assign_internal_groups(
                 client,
                 bundle_id="com.x5studio.app",
-                build_number="191",
+                build_number="192",
                 group_names=("123", "321"),
             )
         self.assertEqual(client.added, [])
